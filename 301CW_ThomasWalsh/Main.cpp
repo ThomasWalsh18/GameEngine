2c28ffd4d3680078b08ca791e8091fc6d3fa2e5c
walsht <walsht@EC214-ECWS-07>
2019-09-17 17:48:37 +0100

    Comments for the game Idea and stuff

--- 301CW_ThomasWalsh/Main.cpp
+++ 301CW_ThomasWalsh/Main.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World" << endl;
+	/*
+	Game Should be:
+	tank management system 
+	- Physics 
+		shooting
+		movement through water and other terrain, moving with damage
+	- Audio
+		Shooting
+		tank moving 
+	- UI
+		Fule
+		Ammo
+		Health
+		Menus
+	- Graphics 
+		The tank itself 
+		other tanks
+		terrain 
+		sky 
+	- Multiplayer 
+	- Gameplay 
+		Resourse management system based on a tank, sort of first person shooter style but with fuel and ammo etc
+		maybe also manage oxygen because you are on a forgien planet and you will lose oxygen when you get shot 
+	*/
+	cout << "Hello World!" << endl;
 	return 0;
 }
