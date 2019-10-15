0c08a33f5f637037eb2237a31ec3839a9c560c05
Thomas Walsh (walsht) <walsht@coventry.ac.uk>
2019-10-15 18:36:14 +0100

    Updated the SDL

--- 301CW_ThomasWalsh/Graphics.cpp
+++ 301CW_ThomasWalsh/Graphics.cpp
@@ -19,10 +19,39 @@ Graphics::~Graphics()
 
 void Graphics::init()
 {
-	std::cout << "Graphics Init" << std::endl;
+	SDL_Init(SDL_INIT_VIDEO);
+
+	window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	surface = SDL_GetWindowSurface(window);
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
+	if (window == NULL)
+	{
+		printf("could not create window: %s\n", SDL_GetError());
+	}
+	else {
+		std::cout << "Graphics Init" << std::endl;
+	}
 }
 
 void Graphics::update()
 {
+	SDL_LockSurface(surface);
+
+	Uint32* pixels = (Uint32*)surface->pixels;
+
+	for (unsigned int x = 0; x < WIDTH; ++x)
+	{
+		for (unsigned int y = 0; y < HEIGHT; ++y)
+		{
+			
+		}
+	}
+
+	SDL_UnlockSurface(surface);
+	//Update the surface
+	SDL_UpdateWindowSurface(window);
+	
+
 	std::cout << "Graphics Updated" << std::endl;
 }
--- 301CW_ThomasWalsh/Graphics.h
+++ 301CW_ThomasWalsh/Graphics.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "subSystem.h"
+#include <glm.hpp>
 #include <SDL.h>
 #include <SDL_main.h>
 #include <iostream>
+
 class Graphics : public subSystem
 {
+private:
+	const int WIDTH = 1600;
+	const int HEIGHT = 900;
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_Surface* surface = nullptr; 
 public:
 	Graphics();
 	~Graphics();
