e7944340b867829d068b20cf7bba8822ce7edaeb
Thomas Walsh (walsht) <walsht@coventry.ac.uk>
2019-11-12 12:51:18 +0000

    Mouse Attempt 3

--- 301CW_ThomasWalsh/UI.cpp
+++ 301CW_ThomasWalsh/UI.cpp
@@ -14,49 +14,7 @@ UI::UI()
 
 }
 
-void UI::wmMouseMove(HWND hwnd, WPARAM wp, LPARAM lp) {
-	POINTS p;
-	HDC hdc;
-	std::cout << " Mouse moved" << std::endl;
-	//if (wp != MK_LBUTTON) return;
-//https://stackoverflow.com/questions/50691580/wm-mousemove-get-x-lparam-and-get-y-lparam-catch-wrong-coordinates
-	p = MAKEPOINTS(lp);
-	hdc = GetDC(hwnd);
-	SelectObject(hdc, g_hpenFore);
-	MoveToEx(hdc, g_oldPoint.x, g_oldPoint.y, 0);
-	LineTo(hdc, p.x, p.y);
-	g_oldPoint = p;
-	ReleaseDC(hwnd, hdc);
-}
-void UI::wmLButtonDown(HWND hwnd, WPARAM wp, LPARAM lp) {
-	g_oldPoint = MAKEPOINTS(lp);
-	SetCapture(hwnd);
-}
 
-void UI::wmLButtonUp(HWND hwnd, WPARAM wp, LPARAM lp) {
-	ReleaseCapture();
-}
-
-LRESULT CALLBACK UI::WindowProcedure(HWND hwnd, UINT message, WPARAM wp, LPARAM lp)
-{
-	switch (message) {
-	case WM_MOUSEMOVE:
-		wmMouseMove(hwnd, wp, lp);
-		break;
-	case WM_LBUTTONDOWN:
-		wmLButtonDown(hwnd, wp, lp);
-		break;
-	case WM_LBUTTONUP:
-		wmLButtonUp(hwnd, wp, lp);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hwnd, message, wp, lp);
-	}
-	return 0;
-}
 UI::~UI()
 {
 	
@@ -80,9 +38,94 @@ void UI::addMainChar(Event* toAdd) {
 	toAdd->eventInfo.affEntities.push_back(MainCharacter);
 }
 
+int WINAPI mouse() {
 
+	HWND hwnd;
+	MSG messages;
+	WNDCLASSEX wincl;
+
+	wincl.style = CS_DBLCLKS;
+	wincl.cbSize = sizeof(WNDCLASSEX);
+
+	wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wincl.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wincl.lpszMenuName = NULL;
+	wincl.cbClsExtra = 0;
+	wincl.cbWndExtra = 0;
+	wincl.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+
+	
+
+	if (GetMessage(&messages, NULL, 0, 0)) {
+		TranslateMessage(&messages);
+		DispatchMessage(&messages);
+	}
+
+	return messages.wParam;
+}
+
+LRESULT CALLBACK UI::WindowProcedure(HWND hwnd, UINT message, WPARAM wp, LPARAM lp) {
+	switch (message) {
+	case WM_MOUSEMOVE:
+		wmMouseMove(hwnd, wp, lp);
+		break;
+	case WM_LBUTTONDOWN:
+		wmLButtonDown(hwnd, wp, lp);
+		break;
+	case WM_LBUTTONUP:
+		wmLButtonUp(hwnd, wp, lp);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);       // Send WM_QUIT
+		break;
+	case WM_KEYDOWN:
+		wmKeyDown(hwnd, wp, lp);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wp, lp);
+	}
+
+	return 0;
+}
+void UI::wmLButtonDown(HWND hwnd, WPARAM wp, LPARAM lp) {
+	g_oldPoint = MAKEPOINTS(lp);
+	SetCapture(hwnd);
+}
+
+void UI::wmLButtonUp(HWND hwnd, WPARAM wp, LPARAM lp) {
+	ReleaseCapture();
+}
+
+void UI::wmKeyDown(HWND hwnd, WPARAM wp, LPARAM lp) {
+	switch (wp) {
+	case VK_ESCAPE:
+		PostMessage(hwnd, WM_QUIT, 0, 0);
+		break;
+	}
+}
+void UI::wmMouseMove(HWND hwnd, WPARAM wp, LPARAM lp) {
+	POINTS p;
+	HDC hdc;
+
+	if (wp != MK_LBUTTON) return;
+
+	p = MAKEPOINTS(lp);
+	hdc = GetDC(hwnd);
+
+	MoveToEx(hdc, g_oldPoint.x, g_oldPoint.y, 0);
+	g_oldPoint = p;
+	ReleaseDC(hwnd, hdc);
+}
 void UI::update()
 {
+
+
+	
+	mouse();
+	
+
+	/*
 	if (GetKeyState('A') & 0x0800) { // to stop toggle
 		Event* left = new Event(EventTypeEnum(0));
 		// dir done in physics eventyally as I can store instead a char of the key that was pressed
@@ -137,7 +180,7 @@ void UI::update()
 		GameEngine::eventQueue.push_back(upward);
 		std::cout << "Event add upward" << std::endl;
 	}
-	if (GetKeyState('Q') & 0x800 ) {
+	if (GetKeyState('Q') & 0x0800 ) {
 		Event* downward = new Event(EventTypeEnum(0));
 		downward->eventInfo.dir = 3;
 		downward->eventInfo.speed = 1;
@@ -145,9 +188,58 @@ void UI::update()
 		GameEngine::eventQueue.push_back(downward);
 		std::cout << "Event add downward" << std::endl;
 	}
-	if (GetMessage(&messages, NULL, 0, 0)) {
-		TranslateMessage(&messages);
-		DispatchMessage(&messages);
+	*/
+}
+
+/*
+void UI::mouseMove(int x, int y)
+{
+	if (firstMouse)
+	{
+		lastX = x;
+		lastY = y;
+		firstMouse = false;
 	}
 
+	float xoffset = x - lastX;
+	float yoffset = lastY - y; // reversed since y-coordinates go from bottom to top
+	lastX = x;
+	lastY = y;
+
+	float sensitivity = 0.3f; // change able sensitivty higher it is the more sensitive. Better to have a large sensitivy so I can see the whole screen
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f) {
+		pitch = 89.0f;
+	}
+	if (pitch < -89.0f) {
+		pitch = -89.0f;
+	}
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+	//force mouse to stay inside the window
+	int win_w = glutGet(GLUT_WINDOW_WIDTH);
+	int win_h = glutGet(GLUT_WINDOW_HEIGHT);
+	if (x < 100 || x > win_w - 100) {  //you can use values other than 100 for the screen edges if you like, kind of seems to depend on your mouse sensitivity for what ends up working best
+		lastX = win_w / 2;   //centers the last known position, this way there isn't an odd jump with your cam as it resets
+		lastY = win_h / 2;
+		glutWarpPointer(win_w / 2, win_h / 2);  //centers the cursor
+	}
+	else if (y < 100 || y > win_h - 100) {
+		lastX = win_w / 2;
+		lastY = win_h / 2;
+		glutWarpPointer(win_w / 2, win_h / 2);
+	}
+	//std::cout << "cameraFront" << glm::to_string(cameraFront) << std::endl;
+	//std::cout << "Camera pos" << glm::to_string(cameraPos) << std::endl;
 }
+*/
--- 301CW_ThomasWalsh/UI.h
+++ 301CW_ThomasWalsh/UI.h
@@ -4,7 +4,7 @@
 #include <windowsx.h>
 #include "GameEngine.h"
 
-class UI: public subSystem
+class UI : public subSystem
 {
 public:
 	UI();
@@ -13,15 +13,19 @@ public:
 	void addMainChar(Event* toAdd);
 	void update();
 	Entity* MainCharacter;
-	
-	HBRUSH g_hbrBack;
-	HPEN   g_hpenFore;
-	POINTS g_oldPoint;
-	MSG messages;
 
-	LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wp, LPARAM lp);
-	void wmMouseMove(HWND hwnd, WPARAM wp, LPARAM lp);
-	void wmLButtonDown(HWND hwnd, WPARAM wp, LPARAM lp);
-	void wmLButtonUp(HWND hwnd, WPARAM wp, LPARAM lp);
+	HBRUSH g_hbrBack;
+	POINTS g_oldPoint;
+	BOOL do_mouse_move = FALSE;
+	//char szClassName[] = "MouseTrack";
+	
+
+	LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+	void wmMouseMove(HWND, WPARAM, LPARAM);
+	void Do_Draw(HDC, WPARAM, LPARAM);
+	void wmLButtonDown(HWND, WPARAM, LPARAM);
+	void wmLButtonUp(HWND, WPARAM, LPARAM);
+	void wmKeyDown(HWND, WPARAM, LPARAM);
+	void wmLButtonDblClk(HWND, WPARAM, LPARAM);
 };
 
